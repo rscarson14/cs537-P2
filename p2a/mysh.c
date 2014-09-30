094051671af4b89efdc5f3648f34d2511cc94073
Robert Carson <rcarson@galapagos-09.cs.wisc.edu>
2014-09-29 23:38:52 -0500

    Have built-ins and other commands running. Working on output redirect and piping

--- p2a/mysh.c
+++ p2a/mysh.c
@@ -9,24 +9,31 @@
 #include <string.h> 
 #include "mysh_utils.h"
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 	(1024)
+
+#define RET_ERROR 	(-1)
+#define RET_EXIT	(1)
+#define RET_OK		(0)
 
 int parse_input(char *line_buffer, char* tokens[]);
 int execute_command(char* tokens[], int num_tokens);
 
-volatile int finished;
+//volatile int finished;
 
 int main(int argc, char* argv[]){
 	
 	int length;
 	int num_tokens;
+	int retval;
+
 	char line_buffer[1024];
 	char* tokens[1024];
 	const char *prompt = "mysh> ";
 	char* command;
-	finished = 0;
 
-	while(!finished){
+	//finished = 0;
+
+	while(1){
 		printf("%s", prompt);
 		fgets(line_buffer, BUFFER_SIZE, stdin);
 
@@ -36,12 +43,25 @@ int main(int argc, char* argv[]){
 			line_buffer[length] = '\0';
 		}
 
-		printf("Buffer = %s\n", line_buffer);
+		//printf("Buffer = %s\n", line_buffer);
 		num_tokens = parse_input(line_buffer, tokens);
-		printf("Number of tokens = %d\n", num_tokens);
+		//printf("Number of tokens = %d\n", num_tokens);
 		if(num_tokens > 0){
-			printf("Command: %s\n", tokens[0]);
-			execute_command(tokens, num_tokens);
+			//printf("Command: %s\n", tokens[0]);
+			retval = execute_command(tokens, num_tokens);
+			switch(retval){
+				case RET_ERROR:
+					fprintf(stderr, "Error!\n");
+					break;
+				case RET_EXIT:
+					//printf("Exiting...\n");
+					exit(0);
+					break;
+				case RET_OK:
+					break;
+				default:
+					break;
+			}
 		}
 	}
 	
--- p2a/mysh_utils.c
+++ p2a/mysh_utils.c
@@ -3,9 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "mysh_utils.h"
 
 #define PWD_BUF_SIZE 1024
+
+int find_pipe(char* tokens[], int num_tokens);
 
 int parse_input(char *line_buffer, char* tokens[]){
 	char *next_token;
@@ -13,10 +17,10 @@ int parse_input(char *line_buffer, char* tokens[]){
 	int index = 0;
 	int token_length = 0;
 	
-	printf("In parse_input: line_buffer = %s\n", line_buffer);
+	//printf("In parse_input: line_buffer = %s\n", line_buffer);
 	next_token = strtok(line_buffer, delims);
 	if(next_token != NULL){
-		printf("token = %s\n", next_token);
+		//printf("token = %s\n", next_token);
 		token_length = strlen(next_token);
 		tokens[index] = (char *) malloc(token_length);
 		strcpy(tokens[index], next_token);
@@ -27,7 +31,7 @@ int parse_input(char *line_buffer, char* tokens[]){
 	}
 
 	while((next_token = strtok(NULL, delims)) != NULL){
-		printf("token = %s\n", next_token);
+		//printf("token = %s\n", next_token);
 		token_length = strlen(next_token);
 		tokens[index] = (char *) malloc(token_length);
 		strcpy(tokens[index], next_token);
@@ -39,14 +43,24 @@ int parse_input(char *line_buffer, char* tokens[]){
 
 int execute_command(char* tokens[], int num_tokens){
 	char* command = tokens[0];
-	char* pwd_buf;
+	char* pwd_buf; // Allocated if a pwd command is executed
+	
+	FILE* redir;
+	
+	pid_t child;
+
 	int redir_overwrite = 0;
 	int redir_append = 0;
-	int pipe = 0;
+	int pipe_index = -1; // index of character in tokens, -1 denotes no pipe
+	int i = 0;
+	int retval = 0;
+	int status; // Used for wait()/waitpid() calls
 
-	if(!strcmp(command, "exit")){
-		exit(0);
+	// Check the simplest command that requires no redirection/piping/etc
+	if(!strcmp(command, "exit")){		
+		return 1;
 	}
+	// Check the next simplest command that requires no redirection/piping/etc
 	else if(!strcmp(command, "cd")){
 		chdir(tokens[1]);
 		pwd_buf = malloc(PWD_BUF_SIZE);
@@ -59,13 +73,56 @@ int execute_command(char* tokens[], int num_tokens){
 	if(!strcmp(tokens[num_tokens - 2], ">")){
 		printf("Output redirection >\n");
 		redir_overwrite = 1;
+		redir = fopen(tokens[num_tokens - 1], "w"); 
+		if(redir == NULL){
+			retval = -1; // error
+			return -1;
+		}
+		child = fork();
+		if(child != 0){
+			waitpid(child, &status, 0);
+		}
+		else{
+			dup2(fileno(stdout), fileno(redir));
+		}
 	}
 	else if(!strcmp(tokens[num_tokens -2], ">>")){
 		printf("Output redirection >>\n");
 		redir_append = 1;
+		redir = fopen(tokens[num_tokens - 1], "wa");
+		if(redir == NULL){
+			retval = -1; // error
+			return -1;
+		}
 	}
+	else if((pipe_index = find_pipe(tokens, num_tokens)) > -1){ // Search for pipe '|'
+		
+	}
+	else{
+		child = fork();
+		if(child != 0){
+			waitpid(child, &status, 0);
+		}
+		else{
+			tokens[num_tokens] = NULL;
+			execvp(command, tokens);
+			exit(-1);
+		}
+	}
+	
+	return status;
+}
 
+int find_pipe(char* tokens[], int num_tokens){
+	int pipe_index = -1;
+	int i = 0;
 
-
-	return 0;
+	for(i = 0; i < num_tokens; i++){
+		if(!strcmp(tokens[i], "|")){
+			pipe_index = i;
+			printf("Pipe at %d\n", pipe_index);
+			i = num_tokens;
+		}
+	}
+	return pipe_index;
 }
